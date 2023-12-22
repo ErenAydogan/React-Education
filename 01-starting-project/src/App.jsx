d8be48b383ef5c8e127f4fa12cd132c47a8fad94
ErenAydogan <erenaydogan@live.fr>
2023-12-22 19:16:36 +0300

    Update App.jsx

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -39,8 +39,6 @@ export default function App() {
           <ul>
             <CoreConcept {...CORE_CONCEPTS[0]}/>
             <CoreConcept {...CORE_CONCEPTS[1]}/>
-            <CoreConcept {...CORE_CONCEPTS[2]}/>
-            <CoreConcept {...CORE_CONCEPTS[3]}/>
           </ul>
         </section>
         <h2>Time to get started!</h2>
